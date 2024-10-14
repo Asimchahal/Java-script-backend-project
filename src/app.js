4c1172c330ebe7bcfcdfda74d8cb618434c06a57
Asimchahal <asimzulfiqar174@gmail.com>
2024-10-14 22:05:37 +0500

    controller for registration

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
-import express, { urlencoded } from "express";
-import cors from 'cors'
+import express from "express";
 import cookieParser from "cookie-parser";
-
-const app=express();
+import cors from 'cors'
+const app =express();
 
 app.use(express());
-app.use(express.json({limit:'16kb'}))
-app.use(urlencoded({extends:true,limit:'16kb'}))
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    Credential:true
-}))
+app.use(cors({origin:process.env.CORS_ORIGIN,Credential:true}))
+app.use(express.json({limit:'16kb'}));
+app.use(express.urlencoded({
+    limit:'16kb',extends:true
+}));
+app.use(cookieParser());
 app.use(express.static('public'))
-app.use(cookieParser())
-export {app};
+
+// routes import
+import userRouter from './routes/user.route.js'
+
+// routes declaration
+app.use('/api/v1/accounts',userRouter)
+export {app}
